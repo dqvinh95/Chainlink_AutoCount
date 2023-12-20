7453a211789f7ce3962907334f5be61eba8dc7ba
dqvinh95 <31183124+dqvinh95@users.noreply.github.com>
2023-12-20 11:36:10 +0700

    Add files via upload

---
+++ AutoCountChainlink.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Count{
+    uint public counted = 0;
+
+    constructor() {}
+    function count() public {
+        counted += 1;
+
+        }
+    }  
